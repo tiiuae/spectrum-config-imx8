4165237a970f44774de57b336a48948b5e4742e1
Ville Ilvonen <ville.ilvonen@unikie.com>
2022-09-21 14:11:20 +0300

    config: out-of-tree build config.nix for imx8
    - either place under spectrum repository root
      or
      spectrum-config in the NIX_PATH
    - for more details, see
    https://spectrum-os.org/doc/build-configuration.html
    
    Signed-off-by: Ville Ilvonen <ville.ilvonen@unikie.com>

---
+++ config.nix
@@ -0,0 +1,11 @@
+{
+  pkgs = import <nixpkgs> {
+    overlays = [(
+      self: super:
+      {
+        kernel = super.linux_imx8.override {};
+      }
+    )];
+    crossSystem = { config = "aarch64-unknown-linux-musl"; };
+  };
+}
