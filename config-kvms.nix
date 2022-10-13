29e76a8c6d74053423bf97299407de3f9f10e8b9
Grigoriy Romanov <grigoriy.romanov@unikie.com>
2022-10-13 10:15:50 +0300

    Implement kvms presence for imx8qxp board
    There are three changes to be done to have kvms enabled:
    kernel patch, uboot patch and kvms binary placed to boot
    partition. Mentioned patches delivered with the configuration
    repo as far as kernel is overriden here.
    
    kvms should be built from the sources in near future but now
    its not implemented. Good news are its possible to use prebuilt
    kvms binaries which are implemented in kvms_bin package.
    Temporarily kvms is rewritten to binary one in configuration
    file and after kvms built from sources will be implemented a
    redefinition could be removed from config file.
    
    Signed-off-by: Grigoriy Romanov <grigoriy.romanov@unikie.com>

---
+++ config-kvms.nix
@@ -0,0 +1,38 @@
+{
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (self: super:
+        {
+          linux_latest = super.linuxKernel.kernels.linux_imx8.override {
+            structuredExtraConfig = with self.lib.kernel; {
+              ATA_PIIX = yes;
+              EFI_STUB = yes;
+              EFI = yes;
+              VIRTIO = yes;
+              VIRTIO_PCI = yes;
+              VIRTIO_BLK = yes;
+              EXT4_FS = yes;
+            };
+            kernelPatches = [{
+              name = "kernel_kvms_5.15.32_imx8";
+              patch = ./patches/kernel_kvms_5.15.32_imx8.patch;
+            }];
+          };
+
+          makeModulesClosure = args: super.makeModulesClosure (args // {
+            rootModules = [ "dm-verity" "loop" ];
+          });
+
+          ubootIMX8QXP = super.ubootIMX8QXP.overrideAttrs ( {patches ? [],...}: {
+            patches = patches ++ [
+              ./patches/uboot-kvms-copy-to-internal-memory.patch
+            ];
+          });
+
+          kvms = super.kvms_bin;
+
+        })
+    ];
+    crossSystem = { config = "aarch64-unknown-linux-musl"; };
+  };
+}
---
+++ imx8qxp/kvms.nix
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: 2022 Unikie
+
+{ config ? import ../../spectrum/nix/eval-config.nix {} }:
+
+let
+  inherit (config) pkgs;
+  uboot = pkgs.ubootIMX8QXP;
+  spectrum = import ../../spectrum/img/live { };
+  kernel = spectrum.rootfs.kernel;
+  kvms = pkgs.kvms;
+in
+
+with pkgs;
+
+stdenvNoCC.mkDerivation {
+  pname = "spectrum-live-imx8qxp.img";
+  version = "0.1";
+
+  unpackPhase = "true";
+
+  nativeBuildInputs = [
+    pkgsBuildHost.util-linux
+    pkgsBuildHost.jq
+    pkgsBuildHost.mtools
+  ];
+
+  buildCommand = ''
+    install -m 0644 ${spectrum} $pname
+    dd if=/dev/zero bs=1M count=6 >> $pname
+    partnum=$(sfdisk --json $pname | grep "node" | wc -l)
+    while [ $partnum -gt 0 ]; do
+      echo '+6M,' | sfdisk --move-data $pname -N $partnum
+      partnum=$((partnum-1))
+    done
+    dd if=${uboot}/flash.bin of=$pname bs=1k seek=32 conv=notrunc
+    IMG=$pname
+    ESP_OFFSET=$(sfdisk --json $IMG | jq -r '
+      # Partition type GUID identifying EFI System Partitions
+      def ESP_GUID: "C12A7328-F81F-11D2-BA4B-00A0C93EC93B";
+      .partitiontable |
+      .sectorsize * (.partitions[] | select(.type == ESP_GUID) | .start)
+    ')
+    mcopy -no -i $pname@@$ESP_OFFSET ${kernel}/dtbs/freescale/imx8qxp-mek.dtb ::/
+    mcopy -no -i $pname@@$ESP_OFFSET ${kvms}/bl1.bin ::/
+    mv $pname $out
+  '';
+}
