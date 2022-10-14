8efa376f18dbfd00bb66d399bd4b5ac4054a8451
Yuri Nesterov <yuriy.nesterov@unikie.com>
2022-10-14 13:38:58 +0000

    imx8qm: add image configuration for imx8qm
    Signed-off-by: Yuri Nesterov <yuriy.nesterov@unikie.com>

---
+++ imx8qm/default.nix
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: 2022 Unikie
+
+{ config ? import ../../spectrum/nix/eval-config.nix {} }: config.pkgs.callPackage (
+{ stdenvNoCC, util-linux, jq, mtools }:
+
+let
+  inherit (config);
+  uboot = config.pkgs.ubootIMX8QM;
+  spectrum = import ../../spectrum/release/live { inherit (config); };
+  kernel = spectrum.rootfs.kernel;
+in
+
+stdenvNoCC.mkDerivation {
+  pname = "spectrum-live-imx8qm.img";
+  version = "0.1";
+
+  unpackPhase = "true";
+
+  nativeBuildInputs = [
+    util-linux
+    jq
+    mtools
+  ];
+
+  buildCommand = ''
+    install -m 0644 ${spectrum} $pname
+    dd if=/dev/zero bs=1M count=6 >> $pname
+    partnum=$(sfdisk --json $pname | grep "node" | wc -l)
+    while [ $partnum -gt 0 ]; do
+      echo '+6M,' | sfdisk --move-data $pname -N $partnum
+      partnum=$((partnum-1))
+    done
+    dd if=${uboot}/flash.bin of=$pname bs=1k seek=32 conv=notrunc
+    IMG=$pname
+    ESP_OFFSET=$(sfdisk --json $IMG | jq -r '
+      # Partition type GUID identifying EFI System Partitions
+      def ESP_GUID: "C12A7328-F81F-11D2-BA4B-00A0C93EC93B";
+      .partitiontable |
+      .sectorsize * (.partitions[] | select(.type == ESP_GUID) | .start)
+    ')
+    mcopy -no -i $pname@@$ESP_OFFSET ${kernel}/dtbs/freescale/imx8qm-mek-hdmi.dtb ::/
+    mcopy -no -i $pname@@$ESP_OFFSET ${config.pkgs.imx-firmware}/hdmitxfw.bin ::/
+    mv $pname $out
+  '';
+}
+) { }
